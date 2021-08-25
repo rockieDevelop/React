f95b451abdad723798e63caec772922f2c7e5644
rockieDevelop <39309609+rockieDevelop@users.noreply.github.com>
2021-08-26 01:02:36 +0200

    beginning of application

---
+++ FirstProject/task-tracker/src/App.js
@@ -0,0 +1,19 @@
+// import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+//Usage of a class instead of a function
+// class App extends React.Component{
+//   render(){
+//     return <h1>class</h1>
+//   }
+// }
+
+export default App;
