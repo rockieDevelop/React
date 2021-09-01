d00c641e2a7f0f1e4d147d5398ca301f93f1aca6
rockieDevelop <39309609+rockieDevelop@users.noreply.github.com>
2021-09-01 19:42:29 +0200

    Finish the UI

--- FirstProject/task-tracker/src/components/Header.js
+++ FirstProject/task-tracker/src/components/Header.js
@@ -2,14 +2,10 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
-    const onClick = () => {
-        alert('Click 1')
-    }
-
     return (
         <header className='header'>
             <h1>{props.title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button color={!props.showAdd ? 'green' : 'blue'} text={!props.showAdd ? 'Add' : 'Close'} onClick={props.onAdd}/>
         </header>
     )
 }
